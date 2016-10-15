9188ed2202649064456af0d88bc1c7a73d2d7297
mansivirani <mansi.virani@gmail.com>
2016-10-15 15:18:35 -0400

    Rankerphrased integrated

--- src/edu/nyu/cs/cs2580/RankerPhrase.java
+++ src/edu/nyu/cs/cs2580/RankerPhrase.java
@@ -1,6 +1,8 @@
 package edu.nyu.cs.cs2580;
 
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
@@ -22,55 +24,63 @@ public class RankerPhrase extends Ranker {
 
   @Override
   public Vector<ScoredDocument> runQuery(Vector<Query> queries, int numResults) {
-    Vector<ScoredDocument> all = new Vector<ScoredDocument>();
+      Vector<ScoredDocument> results = new Vector<ScoredDocument>();
     // @CS2580: fill in your code here.
-    return all;
-  }
 
-  int phrasecompare(String[] Query, int docID) {
+      int numDocs = _indexer.numDocs();
 
-      Document doc = _indexer.getDoc(docID);
-      Vector<String> docwords = ((DocumentFull) doc).getConvertedBodyTokens();
-
-      double score = 0.0;
-      int i = 0;
-      int bigramfreq = 0;
-      int ctr=0;
-
-      if (Query.length == 1)
-          bigramfreq = getfreq(docID);
-
-      else {
-
-          for (int j = 0; j < docwords.size(); j++) {
-
-              
-              while(i!=Query.length-1){
-
-                  if(Query[i]==docwords.get(j)){
-
-                      if(Query[i+1]==docwords.get(j+1)){
-
-                          bigramfreq++;
-                          i++;
-                          j++;
-                      }
-                      else
-                          i++;
-                  }
-                  else
-                      i++;
-              }
-
-              i=0;
-
+      for(Query query : queries) {
+          Vector<ScoredDocument> all = new Vector<ScoredDocument>();
+          for (int i = 0; i < _indexer.numDocs(); ++i) {
+              all.add(scoreDocument(query, i, numDocs));
           }
-
+          Collections.sort(all, Collections.reverseOrder());
+          for (int i = 0; i < all.size() && i < numResults; ++i) {
+              results.add(all.get(i));
+          }
       }
-      return bigramfreq;
-  }
 
-int getfreq(int docID){
-    return 20;
-}
+      return results;
+
+  }
+    private ScoredDocument scoreDocument(Query query, int did, int numDocs) {
+        DocumentFull docFull = (DocumentFull) _indexer.getDoc(did);
+        HashMap<String, Double> docTokenCountMap = docFull.getTokenCountMap();
+
+        int docTokenCount = docFull.getBodyTokens().size();
+
+        double score = 0.0;
+        int i = 0;
+
+        for (int k=0;k<numDocs;++k) {
+
+            if (query._tokens.size() == 1)
+
+                score = docTokenCountMap.get(query);
+
+            else {
+
+                for (int j = 0; j < docTokenCount - 1; j++) {
+
+
+                    while (i != query._tokens.size() - 1) {
+
+                        if (query._tokens.get(i).equals(docFull.getConvertedBodyTokens().get(j))) {
+
+                            if (query._tokens.get(i + 1).equals(docFull.getConvertedBodyTokens().get(j + 1))) {
+
+                                score++;
+                                i++;
+                            } else
+                                i++;
+                        } else
+                            i++;
+                    }
+                    i = 0;
+                }
+            }
+        }
+        return new ScoredDocument(query._query, docFull, score);
+    }
+
 }
