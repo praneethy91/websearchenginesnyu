3d561c1cc3e3e317ecabadc34e817e3689f229fc
praneethy91 <praneethy91@gmail.com>
2016-10-14 20:22:45 -0400

    Fixing small mistake while merging in cosine ranker

--- src/edu/nyu/cs/cs2580/RankerQl.java
+++ src/edu/nyu/cs/cs2580/RankerQl.java
@@ -23,15 +23,17 @@ public class RankerQl extends Ranker {
 
   @Override
   public Vector<ScoredDocument> runQuery(Vector<Query> queries, int numResults) {
-    Vector<ScoredDocument> all = new Vector<ScoredDocument>();
-    for (int i = 0; i < _indexer.numDocs(); ++i) {
-      double totalTermFrequencyInCorpus = _indexer.totalTermFrequency();
-      all.add(scoreDocument(query, i, totalTermFrequencyInCorpus));
-    }
-    Collections.sort(all, Collections.reverseOrder());
     Vector<ScoredDocument> results = new Vector<ScoredDocument>();
-    for (int i = 0; i < all.size() && i < numResults; ++i) {
-      results.add(all.get(i));
+    for(Query query : queries) {
+      Vector<ScoredDocument> all = new Vector<ScoredDocument>();
+      for (int i = 0; i < _indexer.numDocs(); ++i) {
+        double totalTermFrequencyInCorpus = _indexer.totalTermFrequency();
+        all.add(scoreDocument(query, i, totalTermFrequencyInCorpus));
+      }
+      Collections.sort(all, Collections.reverseOrder());
+      for (int i = 0; i < all.size() && i < numResults; ++i) {
+        results.add(all.get(i));
+      }
     }
     return results;
   }
