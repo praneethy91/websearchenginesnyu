cebff9afd2caf385cccc942390e45adaaf4a82af
praneethy91 <praneethy91@gmail.com>
2016-10-16 23:04:36 -0400

    Adding the rank of each file in the html ouput

--- src/edu/nyu/cs/cs2580/HtmlFormatter.java
+++ src/edu/nyu/cs/cs2580/HtmlFormatter.java
@@ -12,6 +12,7 @@ import java.util.Vector;
 public class HtmlFormatter {
   String _htmltemplate = "<table id=\"%s\" border=\"1\" cellpadding=\"5\" cellspacing=\"5\">" +
                           "<tr>" +
+                            "<th>Rank</th>" +
                             "<th>Query</th>" +
                             "<th>Doc ID</th>" +
                             "<th>Doc Title</th>" +
@@ -19,7 +20,7 @@ public class HtmlFormatter {
                           "</tr>" +
                         "</table>";
   private Document _htmlDocument = null;
-  private int _tableCount = 0;
+  private int _tableCount = 1;
   public HtmlFormatter(){
     _htmlDocument = Document.createShell("");
   }
@@ -28,8 +29,10 @@ public class HtmlFormatter {
     _htmlDocument.body().appendElement("h1").text(rankerType.getDesc());
     _htmlDocument.body().append(getHtmlTemplate());
     Elements table = _htmlDocument.select("#" + _tableCount);
+    int rank = 1;
     for(ScoredDocument scoredDocument : scoredDocumentVector) {
-      table.append("<tr><td>" + query._query + "</td><td>" + scoredDocument.getID() + "</td><td>" + scoredDocument.getTitle() + "</td><td>" + scoredDocument.getScore() + "</td></tr>");
+      table.append("<tr><td>" + rank + "</td><td>" + query._query + "</td><td>" + scoredDocument.getID() + "</td><td>" + scoredDocument.getTitle() + "</td><td>" + scoredDocument.getScore() + "</td></tr>");
+      rank++;
     }
     _tableCount++;
     _htmlDocument.body().append("<hr>");
