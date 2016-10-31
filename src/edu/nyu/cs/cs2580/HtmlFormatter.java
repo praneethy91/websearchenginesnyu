bd0dd664d8e01ee44e12e8ac9448dcf925f64106
praneethy91 <praneethy91@gmail.com>
2016-10-31 16:14:23 -0400

    Fixing URL template

--- src/edu/nyu/cs/cs2580/HtmlFormatter.java
+++ src/edu/nyu/cs/cs2580/HtmlFormatter.java
@@ -32,7 +32,7 @@ public class HtmlFormatter {
     Elements table = _htmlDocument.select("#" + _tableCount);
     int rank = 1;
     for(ScoredDocument scoredDocument : scoredDocumentVector) {
-      table.append("<tr><td>" + rank + "</td><td>" + query._query + "</td><td>" + scoredDocument.getID() + "</td><td>" + scoredDocument.getTitle() + "</td><td>" + scoredDocument.getScore() + "</td></td>" + scoredDocument.getUrl() + "</td></tr>");
+      table.append("<tr><td>" + rank + "</td><td>" + query._query + "</td><td>" + scoredDocument.getID() + "</td><td>" + scoredDocument.getTitle() + "</td><td>" + scoredDocument.getScore() + "</td><td>" + scoredDocument.getUrl() + "</td></tr>");
       rank++;
     }
     _tableCount++;
