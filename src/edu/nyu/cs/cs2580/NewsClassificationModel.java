17566e894343a9b841d21b8739e3a4665f181065
praneethy91 <praneethy91@gmail.com>
2016-12-03 07:11:52 -0500

    Now using LibLinear instead of LibSVM for the classsification as it's faster and gives more accurate results for the reuter's corpus.

--- src/edu/nyu/cs/cs2580/NewsClassificationModel.java
+++ src/edu/nyu/cs/cs2580/NewsClassificationModel.java
@@ -1,17 +1,16 @@
 package edu.nyu.cs.cs2580;
 
-import sun.security.jgss.GSSHeader;
+import de.bwaldvogel.liblinear.*;
 import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.functions.LibSVM;
 import weka.core.*;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.sql.Array;
+import java.util.*;
 
 /**
  * Created by Praneeth on 11/30/2016.
@@ -21,6 +20,15 @@ public class NewsClassificationModel {
   private static HashMap<Integer, HashSet<String>> docToCategoryMap = new HashMap<Integer, HashSet<String>>();
   private static HashMap<String, Integer> termsToNumDocsMap = new HashMap<String, Integer>();
   private static int totalDocs = 0;
+  private static int newsPredictionsFailed;
+  private static int newsPredictionsPassed;
+  private static int nonNewsPredictionsFailed;
+  private static int nonNewsPredictionsPassed;
+  private static double[] predictOutputArray;
+  private static FeatureNode[][] trainingSet;
+  private static int numFeatures;
+  private static int numberOfTrainingExamples;
+  private static final String modelDir = "data\\model";
 
   public static void main(String[] args) throws Exception {
     ComputeSVMForDocuments();
@@ -86,7 +94,10 @@ public class NewsClassificationModel {
     corpusDir = new File(fileDirString);
     foundFiles = corpusDir.listFiles(new FilenameFilter() {
       public boolean accept(File dir, String name) {
-        return name.contains("train");
+        return name.contains("lyrl2004_tokens_test_pt0")
+                || name.contains("lyrl2004_tokens_test_pt1")
+                || name.contains("lyrl2004_tokens_test_pt2")
+                || name.contains("train");
       }
     });
 
@@ -129,77 +140,93 @@ public class NewsClassificationModel {
       }
     }
 
-    ArrayList<Attribute> fvWekaAttributes = new ArrayList<Attribute>();
-
     //Start of Weka classification
     //Add words as feature attributes
-    int numFeatures = termsToIntRepresentationMap.size();
-    for(int i = 1; i <= numFeatures; i++) {
-      fvWekaAttributes.add(new Attribute(String.valueOf("feature" + i)));
-    }
+    numFeatures = termsToIntRepresentationMap.size();
+    numberOfTrainingExamples = totalDocs;
 
-    //Add the news categories as labels
-    ArrayList<String> newsClassCategories = new ArrayList<String>();
-    for(int i = 0; i <= 1; i++) {
-      newsClassCategories.add(String.valueOf(i));
-    }
-
-    fvWekaAttributes.add(new Attribute("NewsClass", newsClassCategories));
-
-
+    predictOutputArray = new double[numberOfTrainingExamples];
+    trainingSet = new FeatureNode[numberOfTrainingExamples][];
 
     //Filling the training set with instances now.
     foundFiles = corpusDir.listFiles(new FilenameFilter() {
       public boolean accept(File dir, String name) {
-        return name.contains("train");
+        return name.contains("lyrl2004_tokens_test_pt0")
+                || name.contains("lyrl2004_tokens_test_pt1")
+                || name.contains("lyrl2004_tokens_test_pt2")
+                || name.contains("train");
       }
     });
 
-    Instances isTrainingSet = null;
+    int index = 0;
     for (File file: foundFiles) {
-      isTrainingSet = CreateInstances(fvWekaAttributes, numFeatures, file);
+      index = PopulateTrainingSet(file, index, true, null);
     }
 
-    //Filling the testing set with instances now.
+    Problem problem = new Problem();
+    problem.l = trainingSet.length;
+    problem.n = numFeatures;
+    problem.x = trainingSet;
+    problem.y = predictOutputArray;
+
+    SolverType solver = SolverType.L2R_L2LOSS_SVC_DUAL; // -s 2
+    double C = 32; // cost of constraints violation
+    double eps = 0.01; // stopping criteria
+
+    Parameter parameter = new Parameter(solver, C, eps);
+    Model model = Linear.train(problem, parameter);
+    File modelSaveFile = new File(modelDir + "\\EnvironmentNatureTravelTourism");
+    modelSaveFile.getParentFile().mkdirs();
+    modelSaveFile.createNewFile();
+    model.save(modelSaveFile);
+    model = model.load(modelSaveFile);
+
+    //Evaluating the test data now
     foundFiles = corpusDir.listFiles(new FilenameFilter() {
       public boolean accept(File dir, String name) {
-        return name.contains("test");
+        return name.contains("lyrl2004_tokens_test_pt3");
       }
     });
 
-    Instances isTestingSet = null;
     boolean h = false;
+    index = 0;
     for (File file: foundFiles) {
       if(!h) {
-        isTestingSet = CreateInstances(fvWekaAttributes, numFeatures, file);
+        index = PopulateTrainingSet(file, index, false, model);
       }
       h = true;
     }
 
-    // Training model naive bayes
-    Classifier cModel = (Classifier)new NaiveBayes();
-    cModel.buildClassifier(isTrainingSet);
-
-    //Cross-validation evaluation
-    Evaluation eval = new Evaluation(isTrainingSet);
-    eval.evaluateModel(cModel, isTestingSet);
+    //Evaluation
+    //Evaluation eval = new Evaluation(isTrainingSet);
+    //eval.evaluateModel(cModel, isTestingSet);
 
     //Print evaluation summary
-    String strSummary = eval.toSummaryString();
-    System.out.println(strSummary);
+    //String strSummary = eval.toSummaryString();
+    //System.out.println(strSummary);
+
+    System.out.print(nonNewsPredictionsPassed + " ");
+    System.out.print(nonNewsPredictionsFailed);
+    System.out.println();
+    System.out.print(newsPredictionsFailed + " ");
+    System.out.print(newsPredictionsPassed);
+
+
+    // double[][] cmMatrix = eval.confusionMatrix();
+    /*for (int i = 0; i < cmMatrix.length; i++) {
+      for (int j = 0; j < cmMatrix[i].length; j++) {
+        System.out.print(cmMatrix[i][j] + " ");
+      }
+      System.out.println();
+    }*/
 
     //TODO: (confusion matrix) double[][] cmMatrix = eTest.confusionMatrix();
   }
 
-  private static Instances CreateInstances(ArrayList<Attribute> fvWekaAttributes, int numFeatures, File file) throws IOException {
-
-    // Create an empty training set
-    Instances isTrainingSet = new Instances("Rel", fvWekaAttributes, 10);
-    // Set class index
-    isTrainingSet.setClassIndex(fvWekaAttributes.size() - 1);
-
+  private static int PopulateTrainingSet(File file, int index, boolean isTrain, Model model) throws Exception {
     BufferedReader br = new BufferedReader(new FileReader(file));
     String line;
+    HashSet<String> categories = new HashSet<String>(Arrays.asList("GENV", "GTOUR", "GWEA"));
     while((line = br.readLine()) != null) {
       if(line.trim().equals("")) {
         continue;
@@ -213,24 +240,28 @@ public class NewsClassificationModel {
 
         br.readLine(); // Skip .W line
         String[] wordsInDoc = br.readLine().split("\\s+");
-
-        //
-        Instance iExample = new SparseInstance(numFeatures + 1);
+        List<FeatureNode> featureNodes = new LinkedList<FeatureNode>();
 
         //TODO: Get way to get classification for each specific label
-        int docCategoryFeature = docToCategoryMap.get(docID).contains("GHEA") ? 1 : 0;
+        HashSet<String> intersection = new HashSet<>(categories);
+        intersection.retainAll(docToCategoryMap.get(docID));
+        double docCategoryFeature = intersection.size() > 0 ? 1 : 0;
 
-        //Storing in last index the class label
-        iExample.setValue((Attribute) fvWekaAttributes.get(numFeatures), String.valueOf(docCategoryFeature));
+        if(isTrain) {
+          //Storing in label
+          predictOutputArray[index] = docCategoryFeature;
+        }
+
         int i = 0;
         while(i < wordsInDoc.length) {
-          String word = wordsInDoc[i];
+          String word = wordsInDoc[i].trim();
           int j = i;
           double termFrequency = 1;
           while(j + 1 < wordsInDoc.length && wordsInDoc[j + 1].equals(word)) {
             termFrequency++;
             j++;
           }
+
           i = ++j;
           if(!termsToIntRepresentationMap.containsKey(word)) {
             continue;
@@ -240,14 +271,43 @@ public class NewsClassificationModel {
           double termsInDoc = wordsInDoc.length;
           double wordInNumDocs = termsToNumDocsMap.get(word);
           //TF-IDF term
-          double featureWeight = (termFrequency/termsInDoc)*Math.log(totalDocs/wordInNumDocs);
+          double featureWeight = (termFrequency/termsInDoc)*Math.log(totalDocs/wordInNumDocs)/5;
 
           //Storing in the index the label
-          iExample.setValue((Attribute) fvWekaAttributes.get(wordId), featureWeight);
+          featureNodes.add(new FeatureNode(wordId + 1, featureWeight));
         }
-        isTrainingSet.add(iExample);
+
+        Collections.sort(featureNodes, new Comparator<FeatureNode>() {
+          @Override
+          public int compare(FeatureNode o1, FeatureNode o2) {
+            return o1.getIndex() > o2.getIndex() ? 1 : -1;
+          }
+        });
+
+        FeatureNode[] featuresArr = featureNodes.toArray(new FeatureNode[featureNodes.size()]);
+
+        if(isTrain) {
+          trainingSet[index] = featuresArr;
+        }
+        else {
+          Feature[] toPredictInstance = featuresArr;
+          double actualDocClass = docCategoryFeature;
+          double predictedDocClass = Linear.predict(model, toPredictInstance);
+
+          if (actualDocClass == 1.0 && predictedDocClass == 1.0) {
+            newsPredictionsPassed++;
+          } else if (actualDocClass == 1.0 && predictedDocClass == 0.0) {
+            newsPredictionsFailed++;
+          } else if (actualDocClass == 0.0 && predictedDocClass == 0.0) {
+            nonNewsPredictionsPassed++;
+          } else if (actualDocClass == 0.0 && predictedDocClass == 1.0) {
+            nonNewsPredictionsFailed++;
+          }
+        }
+        index++;
+
       }
     }
-    return isTrainingSet;
+    return index;
   }
 }
