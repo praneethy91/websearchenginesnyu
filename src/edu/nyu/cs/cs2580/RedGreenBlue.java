79f28a3c01753a0d75b28d815c4f0c50d183047c
praneethy91 <praneethy91@gmail.com>
2016-12-15 05:08:27 -0500

    Adding code for coloring and the topic presentation in the UI

---
+++ src/edu/nyu/cs/cs2580/RedGreenBlue.java
@@ -0,0 +1,10 @@
+package edu.nyu.cs.cs2580;
+
+/**
+ * Created by Praneeth on 12/15/2016.
+ */
+public class RedGreenBlue {
+  public int red;
+  public int green;
+  public int blue;
+}
--- src/edu/nyu/cs/cs2580/TopicInfo.java
+++ src/edu/nyu/cs/cs2580/TopicInfo.java
@@ -22,4 +22,43 @@ public class TopicInfo {
   public void setPolarity(double polarity) {
     _polarity = polarity;
   }
+
+  public String getRGB() {
+    RedGreenBlue rgb = getColor();
+    return String.format("rgb(%s,%s,%s)", rgb.red, rgb.green, rgb.blue);
+  }
+
+  @Override
+  public String toString() {
+    return getTopic() + ": " + getPolarity();
+  }
+
+  private RedGreenBlue getColor() {
+    RedGreenBlue rgb = new RedGreenBlue();
+    rgb.red = 255;
+    rgb.green = 255;
+    rgb.blue = 0;
+    if(_polarity >= 0 && _polarity <= 0.5) {
+      int redToDecrease = (int)Math.floor((_polarity/0.5) *255);
+      rgb.red = rgb.red - redToDecrease;
+    }
+    else if(_polarity < 0 && _polarity >=-0.5) {
+      int greenToDecrease = (int)Math.floor((-_polarity/0.5) *255);
+      rgb.green = rgb.green - greenToDecrease;
+    }
+    else if(_polarity > 0.5) {
+      rgb = new RedGreenBlue();
+      rgb.red = 0;
+      rgb.green = 255;
+      rgb.blue = 0;
+    }
+    else {
+      rgb = new RedGreenBlue();
+      rgb.red = 255;
+      rgb.green = 0;
+      rgb.blue = 0;
+    }
+
+    return rgb;
+  }
 }
